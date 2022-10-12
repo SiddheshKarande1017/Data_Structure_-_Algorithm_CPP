6e65a1c20b4ad5eae1d2691761ac9425e925ed6b
SiddheshKarande1017 <85099191+SiddheshKarande1017@users.noreply.github.com>
2022-10-13 00:09:56 +0530

    Time: 28 ms (5.84%), Space: 24.1 MB (5.01%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,45 +1,29 @@
-class Solution {
-public:
-   // int t1=INT_MAX,t2=INT_MAX,t3=INT_MAX;
-    int costtic(vector<int> days, vector<int> costs,int i,int d, int prev, vector<int>&dp)
+class Solution
+{
+    public:
+       	// int t1=INT_MAX,t2=INT_MAX,t3=INT_MAX;
+        int costtic(vector<int> days, vector<int> costs, int i, int d, int prev, vector< int > &dp)
+        {
+            if (i >= days.size()) return 0;
+            if (dp[i] != -1) return dp[i];
+            int t1 = costs[0] + costtic(days, costs, i + 1, 1, days[i], dp);
+            int y = i;
+            for (; y < days.size(); y++)
+            {
+                if (days[y] >= days[i] + 7) break;
+            }
+
+            int t2 = costs[1] + costtic(days, costs, y, 7, days[i], dp);
+            for (; y < days.size(); y++)
+            {
+                if (days[y] >= days[i] + 30) break;
+            }
+            int t3 = costs[2] + costtic(days, costs, y, 30, days[i], dp);
+            return dp[i] = min(t1, min(t2, t3));
+        }
+    int mincostTickets(vector<int> &days, vector<int> &costs)
     {
-        if(i>=days.size()) return 0;
-//         if(i==days.size()-1)
-//         {
-//             int tk=INT_MAX;
-//             if(days[i]>=prev+1)
-//         {
-//             tk=min(tk,costs[0]);
-            
-//         }
-//         if(days[i]>=prev+7)
-//         {
-//             tk=min(tk,costs[1]) ;
-//         }
-//         if(days[i]>=prev+30)
-//         {
-//          tk=min(tk,costs[2]);
-//         }
-//            return tk; 
-//         }
-            if(dp[i]!=-1) return dp[i];
-           int t1=costs[0]+costtic(days,costs,i+1,1,days[i],dp);
-            int y=i;
-        for(;y<days.size();y++)
-        {
-            if(days[y]>=days[i]+7) break;
-        }
-        
-        int t2=costs[1]+costtic(days,costs,y,7,days[i],dp);
-        for(;y<days.size();y++)
-        {
-            if(days[y]>=days[i]+30) break;
-        }
-         int t3=costs[2]+costtic(days,costs,y,30,days[i],dp);
-        return dp[i]=min(t1,min(t2,t3));
-    }
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1,-1);
-        return costtic(days,costs,0,0,-1,dp);
+        vector<int> dp(days.size() + 1, -1);
+        return costtic(days, costs, 0, 0, -1, dp);
     }
 };
