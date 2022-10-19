f5a706aff15e29fc01de2a5fefd4f8d87b141e90
SiddheshKarande1017 <85099191+SiddheshKarande1017@users.noreply.github.com>
2022-10-19 09:23:42 +0530

    Time: 31 ms (30.85%), Space: 13.7 MB (5.47%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string,int> mp;
+        for(auto it:words)
+        {
+            mp[it]++;
+        }
+        priority_queue<pair<int,string>>p1;
+        for(auto it:mp)
+        {
+            p1.push({it.second,it.first});
+        }
+        vector<string> ans;
+        map<int,vector<string>>mp1;
+        int prev=p1.top().first;
+        for(int i=0;i<k;i++)
+        {
+            if(p1.empty()) break;
+            if(p1.top().first==prev) i--;
+            mp1[p1.top().first].push_back(p1.top().second);
+            prev=p1.top().first;
+            p1.pop();
+            
+        }
+       // map<int, string>::reverse_iterator it;
+    cout<<mp1.begin()->second.size();
+         for (auto it = mp1.rbegin(); it != mp1.rend(); it++) {
+        sort(it->second.begin(),it->second.end());
+        for(auto u:it->second)
+        {
+            ans.push_back(u);
+        }
+     }
+        vector<string> jj;
+        for(int i=0;i<k;i++)
+        {
+            jj.push_back(ans[i]);
+        }
+        return jj;
+  
+        
+        
+    }
+};
