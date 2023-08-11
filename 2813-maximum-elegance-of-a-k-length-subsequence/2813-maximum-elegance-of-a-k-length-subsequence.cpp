da4c29993a5077d643a3aa505c25533f4d2170bb
SiddheshKarande1017 <85099191+SiddheshKarande1017@users.noreply.github.com>
2023-08-11 15:27:23 +0530

    Time: 467 ms (53.76%), Space: 180 MB (62.25%) - LeetHub

--- 2813-maximum-elegance-of-a-k-length-subsequence/2813-maximum-elegance-of-a-k-length-subsequence.cpp
+++ 2813-maximum-elegance-of-a-k-length-subsequence/2813-maximum-elegance-of-a-k-length-subsequence.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     
-    long long findMaximumElegance(vector<vector<int>>& v, int k) {
-        int n = v.size();
-        sort(v.rbegin(), v.rend()); 
-        long long ans = 0;
-        map<int, int> m;
-        for(int i = 0; i < k; i++){
-            ans += v[i][0];
-            m[v[i][1]]++;
+    long long findMaximumElegance(vector<vector<int>>& items, int k) {
+        sort(items.begin(), items.end(), greater<vector<int>>());
+        long long ans=0;
+        map<int, int> mp;
+        long long res=0;
+        for(int i=0;i<k;i++)
+        {
+            ans+=items[i][0];
+            mp[items[i][1]]++;
         }
-        long long sz = m.size();
-        ans += (sz * sz);
-        long long res = ans;
-        int j = k - 1;
-        for(int i = k; i < n; i++){
-            if(m.count(v[i][1]) == 0){ 
-                while(j >= 0 && m[v[j][1]] < 2) j--; 
-                if(j < 0) break;
-                m[v[j][1]]--;
-                m[v[i][1]]++; 
-                res -= v[j][0];
-                res += v[i][0];
-                res -= sz*sz;
-                sz++;
-                res += sz*sz;
+        
+        ans+=(mp.size()*mp.size());
+        long long sz=mp.size();
+        res=ans;
+        int j=k-1;
+        for(int i=k;i<items.size();i++)
+        {
+            if(mp.find(items[i][1])==mp.end())
+            {
+                while(j>=0 && mp[items[j][1]]<2) j--;
+                if(j<0) break;
+                mp[items[j][1]]--;
+                mp[items[i][1]]++;
+                
+                res-=items[j][0];
+                res+=items[i][0];
+                res-=(sz*sz);
+                sz=mp.size();
+                res+=(sz*sz);
                 j--;
-                ans = max(ans, res); 
+                ans=max(ans,res);
             }
         }
-        return max(ans, res);
+         return ans;
     }
 };
